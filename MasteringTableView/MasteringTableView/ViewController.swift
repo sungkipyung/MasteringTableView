d2daa91f7af3d8c2bf918033a729ea7eb219d98e
hothead <sungkipyung@gmail.com>
2018-05-24 08:32:12 +0900

    scrollToBottom

--- MasteringTableView/MasteringTableView/ViewController.swift
+++ MasteringTableView/MasteringTableView/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     private let updateQueue = DispatchQueue.init(label: "updateQueue")
     private let keyGenQueue = DispatchQueue.init(label: "keyGenQueue")
     
+    private var tableViewLoadOperationOn = false
+    
     private func nextKey() -> UInt64 {
         self.key = self.key + 1
         return self.key
@@ -39,29 +41,48 @@ class ViewController: UIViewController {
         return items
     }
     
-    private func insertItems(items: [String]) {
-        // 정렬도 해야지? 그지? 응?
+    private func insertItems(items: [String], useReload: Bool = false) {
         DispatchQueue.main.async {
-            UIView.setAnimationsEnabled(false)
-            self.tableView.beginUpdates()
-            let startRow = self.items.count
-            var indexPathes = [IndexPath]()
-            for i in 0..<items.count {
-                indexPathes.append(IndexPath(row: i + startRow, section: 0))
+            if useReload {
+                self.items.append(contentsOf: items)
+                self.tableView.reloadData()
+                self.scrollToBottom()
+            } else {
+                UIView.setAnimationsEnabled(false)
+                self.tableView.beginUpdates()
+                let startRow = self.items.count
+                var indexPathes = [IndexPath]()
+                for i in 0..<items.count {
+                    indexPathes.append(IndexPath(row: i + startRow, section: 0))
+                }
+                self.items.append(contentsOf: items)
+                
+                self.tableView.insertRows(at: indexPathes, with: UITableViewRowAnimation.automatic)
+                self.tableView.endUpdates()
+                UIView.setAnimationsEnabled(true)
+                self.scrollToBottom()
             }
-            self.items.append(contentsOf: items)
             
-            self.tableView.insertRows(at: indexPathes, with: UITableViewRowAnimation.automatic)
-            self.tableView.endUpdates()
-            UIView.setAnimationsEnabled(true)
-            let row = self.items.count - 1
+        }
+    }
+    func scrollToBottom() {
+        let row = self.items.count - 1
+        if row > 0 {
             self.tableView.scrollToRow(at: IndexPath.init(row: row, section: 0), at: UITableViewScrollPosition.bottom, animated: false)
-            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.observe(\UITableView.hasUncommittedUpdates, options: .new) { (tableView, changed) in
+            if let value = changed as? Bool {
+                if value {
+                    print("hasUncommittedUpdates : on")
+                } else {
+                    print("hasUncommittedUpdates : off")
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -70,23 +91,36 @@ class ViewController: UIViewController {
     }
 
     @IBAction func touchUpInsertButton(_ sender: UIButton) {
+
+        // 일반적인 케이스 (normal case)
+//        insertQueue.async { [weak self] in guard let sSelf = self else { return }
+//            let newItems = sSelf.generateItems(count: sSelf.itemCountForNewGen)
+//            sSelf.insertItems(items: newItems)
+//        }
         
+        // crash??
         insertQueue.async { [weak self] in guard let sSelf = self else { return }
-            let newItems = sSelf.generateItems(count: sSelf.itemCountForNewGen)
-            
-            sSelf.insertItems(items: newItems)
+            for i in 0..<sSelf.itemCountForNewGen {
+                let newItems = sSelf.generateItems(count: i + 1)
+                sSelf.insertItems(items: newItems, useReload: true)
+            }
         }
     }
     
     @IBAction func touchUpDeleteButton(_ sender: UIButton) {
-        deleteQueue.async { [weak self] in guard let sSelf = self else { return }
-            
+        deleteQueue.async {
+            DispatchQueue.main.async { [weak self] in guard let sSelf = self else { return }
+                sSelf.items.removeAll()
+                sSelf.tableView.reloadData()
+            }
         }
     }
     
     @IBAction func touchUpUpdateButton(_ sender: UIButton) {
-        updateQueue.async { [weak self] in guard let sSelf = self else { return }
-            
+        updateQueue.async {
+            DispatchQueue.main.async { [weak self] in guard let sSelf = self else { return }
+                sSelf.tableView.reloadData()
+            }
         }
     }
     
@@ -105,25 +139,46 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
+        let count = 1
+        if count == 0 {
+            tableViewLoadOperationOn = false
+        }
         print("numberOfSections")
+        
+        tableViewLoadOperationOn = true
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("numberOfRowsInSection: \(section)")
-        return items.count
+        let count = items.count
+        
+        if count == 0 {
+            tableViewLoadOperationOn = false
+        }
+        
+        return count
     }
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         print("heightForRowAtIndexPath : \(indexPath.row)")
+        if self.items.count - 1 == indexPath.row {
+            tableViewLoadOperationOn = false
+        }
         return 64
     }
     
+    /**
+     이 2개는 delegate의 호출 수가 다르다.
+     tableViewLoadOperationOn off 여부를 판단하기 위해서는 forIndexPath를 사용하면 안된다.(tableView:heightForRowAt:indexPath 3번씩 호출됨)
+     tableView.dequeueReusableCell forIndexPath
+     tableView.dequeueReusableCell
+     */
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print("cellForRowAtIndexPath : \(indexPath.row)")
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageTableViewCell", for: indexPath)
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageTableViewCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageTableViewCell", for: indexPath)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageTableViewCell")!
         
         if let messageCell = cell as? MessageTableViewCell {
             // 크래시가 발생하는 부분이 있을까?
@@ -138,7 +193,7 @@ extension ViewController: UITableViewDelegate {
         if editingStyle == .delete {
             tableView.beginUpdates()
             self.items.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.right)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             tableView.endUpdates()
         } else {
             let newItem = self.generateItems(count: 1).first!
@@ -147,6 +202,7 @@ extension ViewController: UITableViewDelegate {
             tableView.insertRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             tableView.endUpdates()
+            self.scrollToBottom()
         }
     }
     
@@ -167,7 +223,7 @@ extension ViewController: UITableViewDelegate {
         var deleteRowAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "Delete", handler:{action, indexpath in
             tableView.beginUpdates()
             self.items.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.right)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             tableView.endUpdates()
         });
         
